0d74b030af23498caffae310b53d17348a6f5ad8
Hendryque <dakqr20y6ecy@blurmail.net>
2018-06-18 14:07:00 +0200

    static site

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Weiterleiten&#160;&hellip;</title>
+		<meta http-equiv="refresh" content="0;url=/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/feed/atom/";
+		</script>
+
+		<p>Du wirst auf <a href="/feed/atom/">/feed/atom/</a> weitergeleitet</p>
+	</body>
+</html>
