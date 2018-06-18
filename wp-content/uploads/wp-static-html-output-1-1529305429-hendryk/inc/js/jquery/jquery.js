d0eac574b39c7ad5589672b8c8a877a8f8e2deac
Hendryque <dakqr20y6ecy@blurmail.net>
2018-06-18 15:06:10 +0200

    images fixed

---
+++ wp-content/uploads/wp-static-html-output-1-1529305429-hendryk/inc/js/jquery/jquery.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<base href="https://www.silvesterimschloss.eu" />
+
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access /inc/js/jquery/jquery.js
+on this server.</p>
+<hr>
+<address>Apache/2.2.15 (CentOS) Server at www.silvesterimschloss.eu Port 443</address>
+</body></html>
